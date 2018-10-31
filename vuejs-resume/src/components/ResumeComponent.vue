34efdbc5ad8b5c95f7f325a7440a54fbc93059d2
fullheightcoding <mark.dodgson.developer@gmail.com>
2018-10-31 18:30:42 +1100

    Just a quick scaffold including resume component.

---
+++ vuejs-resume/src/components/ResumeComponent.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+    <div>
+      <h2>
+        Personal Details
+      </h2>
+    </div>
+
+    <div>
+      <h2>
+        Profile
+      </h2>
+    </div>
+
+    <div>
+      <h2>
+        Skills and Abilities
+      </h2>
+    </div>
+
+    <div>
+      <h2>
+        Employment History
+      </h2>
+    </div>
+
+    <div>
+      <h2>
+        Projects
+      </h2>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumeComponent',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
