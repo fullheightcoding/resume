2953cda1bead8a55b2b2d89b280caf4f8804069b
fullheightcoding <mark.dodgson.developer@gmail.com>
2018-11-01 12:03:13 +1100

    Add basic bootstrap starter.

--- vuejs-resume/src/components/ResumeComponent.vue
+++ vuejs-resume/src/components/ResumeComponent.vue
@@ -1,38 +1,63 @@
 <template>
-  <div>
-    <h1>{{ msg }}</h1>
+  <div class="container">
+      <!-- <h1>{{ msg }}</h1> -->
 
-    <div>
-      <h2>
-        Personal Details
-      </h2>
-    </div>
+      <div>
+        <div class="row">
+          <h2>Personal Details</h2>
 
-    <div>
-      <h2>
-        Profile
-      </h2>
-    </div>
+          <table class="table table-striped table-bordered table-sm">
+            <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Name:</td>
+                <td>Mark Dodgson</td>
+              </tr>
+              <tr>
+                <td>Address:</td>
+                <td>Unit 2 /  52 Crookston Rd, Reservoir, Victoria 3032</td>
+              </tr>
+              <tr>
+                <td>Mobile:</td>
+                <td>0433547979</td>
+              </tr>
+              <tr>
+                <td>Email:</td>
+                <td>mark.dodgson.developer@gmail.com</td>
+              </tr>
+              <tr>
+                <td>LinkedIn:</td>
+                <td><a href="https://www.linkedin.com/in/mark-dodgson-1a37a2a5">LinkedIn</a></td>
+              </tr>
+              <tr>
+                <td>GitHub:</td>
+                <td><a href="https://github.com/fullheightcoding">GitHub</a></td>
+              </tr>
+            </tbody>
+          </table>
 
-    <div>
-      <h2>
-        Skills and Abilities
-      </h2>
-    </div>
+        </div>
 
-    <div>
-      <h2>
-        Employment History
-      </h2>
-    </div>
+        <div class="row">
+          <h2>Skills and Abilities</h2>
+        </div>
 
-    <div>
-      <h2>
-        Projects
-      </h2>
-    </div>
+        <div class="row">
+          <h2>Employment History</h2>
+        </div>
 
-  </div>
+        <div class="row">
+          <h2>Projects</h2>
+        </div>
+
+      </div>
+
+    </div><!-- /.container -->
 </template>
 
 <script>
@@ -58,5 +83,13 @@ li {
 }
 a {
   color: #42b983;
+}
+/* from bootstrap starter-template */
+body {
+  padding-top: 50px;
+}
+.starter-template {
+  padding: 40px 15px;
+  text-align: center;
 }
 </style>
